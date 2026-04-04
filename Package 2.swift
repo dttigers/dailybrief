261ce6aefa1dcbae40be5eeb7cbe346cacf9cd98
Jameson Morrill <jameson.morrill@icloud.com>
2026-04-04 12:10:46 -0600

    feat: add initial project structure with schemes and package dependencies

---
+++ Package 2.swift
@@ -0,0 +1,45 @@
+// swift-tools-version: 6.2
+import PackageDescription
+
+let package = Package(
+    name: "DailyBrief",
+    platforms: [.macOS(.v14)],
+    products: [
+        .library(name: "JarvisCore", targets: ["JarvisCore"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
+    ],
+    targets: [
+        .target(
+            name: "JarvisCore",
+            dependencies: [
+                .product(name: "GRDB", package: "GRDB.swift"),
+            ],
+            path: "Sources/JarvisCore"
+        ),
+        .executableTarget(
+            name: "DailyBrief",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                "JarvisCore",
+            ],
+            linkerSettings: [
+                .linkedFramework("EventKit"),
+                .linkedFramework("CoreGraphics"),
+                .linkedFramework("CoreText"),
+            ]
+        ),
+        .executableTarget(
+            name: "DailyBriefMonitor",
+            dependencies: [
+                "JarvisCore",
+            ],
+            linkerSettings: [
+                .linkedFramework("SwiftUI"),
+                .linkedFramework("AppKit"),
+            ]
+        ),
+    ]
+)
